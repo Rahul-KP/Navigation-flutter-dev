c2962d13eaaa68aeb6287925ee75b706c4625d0f
Amitash M S <amitu895@gmail.com>
2022-11-14 16:01:36 +0530

    Separated profile buttons' screen from main code

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,8 @@
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: LoginScreen(),
   ));
-}
-
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
-  Widget Btn(String text_) {
-    return Container(
-        width: 200,
-        padding: const EdgeInsets.all(10),
-        margin: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-            color: Colors.amber,
-            border: Border.all(color: Colors.amber),
-            borderRadius: const BorderRadius.all(Radius.circular(10))),
-        child: Center(
-          child: Text(
-            text_,
-            style: const TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Navigation'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Vertical alignment
-            children: [Btn('AMBULANCE DRIVER'), Btn('USER')],
-          ),
-        ));
-  }
 }
