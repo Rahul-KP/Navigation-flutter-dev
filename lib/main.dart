4f704e94b6b974a393335dba390017c42580a11a
Rahul K P <rahulkp01aug@gmail.com>
2022-11-19 15:14:15 +0530

    demonstrating git

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'login_screen.dart';
 import 'get_location.dart';
-
+//demonstarting git to sumedh
 void main() {
   runApp(const MaterialApp(
     home: Pos(),
