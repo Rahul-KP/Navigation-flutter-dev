684ea7bbf1ba5844e6d78edaf114265000f8d9bf
Rahul K P <rahulkp01aug@gmail.com>
2022-11-24 13:23:07 +0530

    trying search container for later overlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'search.dart';
 // import 'login_screen.dart';
 import 'get_location.dart';
 //demonstarting git to sumedh
 void main() {
   runApp(const MaterialApp(
-    home: Pos(),
+    // home: Pos(),
+    home: SearchWidget(),
   ));
 }
