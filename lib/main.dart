30c0f99f78d80b476a7db4515c9afaca0a6bc57b
Rahul K P <rahulkp01aug@gmail.com>
2022-11-24 15:00:53 +0530

    Added search widget as a class

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'get_location.dart';
 //demonstarting git to sumedh
 void main() {
   runApp(const MaterialApp(
-    // home: Pos(),
-    home: SearchWidget(),
+    home: Pos(),
+    // home: SearchWidget(),
   ));
 }
