2e5c9b826271997129688ae8c10f21e1739548cb
Sumedh Hagaldivte <sumedh.hagaldivte@gmail.com>
2022-11-25 17:36:21 +0530

    Starter page ui now has background images and matches the theme . there is no app bar

--- lib/starter.dart
+++ lib/starter.dart
@@ -9,141 +9,174 @@ class loginpg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(90.0),
-        child: AppBar(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(20),
-            bottomRight: Radius.circular(20),
-          )),
-          centerTitle: true,
-          title: Text("Choose Your Role",
-          style: GoogleFonts.bebasNeue(
-            fontSize: 30,
-          )
-          ),
-          backgroundColor: Colors.green,
-          actions: [
-            PopupMenuButton(
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  value: 0,
-                  child: Text(
-                    "Ambulance Driver",
-                    style: GoogleFonts.bebasNeue(),
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/ambiBg.jpg"), fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color.fromARGB(221, 24, 24, 24),
+              Color.fromARGB(221, 0, 0, 0)
+            ])),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          // appBar: PreferredSize(
+          //   preferredSize: Size.fromHeight(90.0),
+          //   child: AppBar(
+          //     shape: const RoundedRectangleBorder(
+          //         borderRadius: BorderRadius.only(
+          //       bottomLeft: Radius.circular(20),
+          //       bottomRight: Radius.circular(20),
+          //     )),
+          //     centerTitle: true,
+          //     title: Text("Choose Your Role",
+          //     style: GoogleFonts.bebasNeue(
+          //       fontSize: 30,
+          //     )
+          //     ),
+          //     backgroundColor: Colors.green,
+          //     actions: [
+          //       PopupMenuButton(
+          //         itemBuilder: (context) => [
+          //           PopupMenuItem(
+          //             value: 0,
+          //             child: Text(
+          //               "Ambulance Driver",
+          //               style: GoogleFonts.bebasNeue(),
+          //             ),
+          //           ),
+          //           PopupMenuItem(
+          //             value: 1,
+          //             child: Text("User"),
+          //           ),
+          //           PopupMenuItem(
+          //             value: 2,
+          //             child: Text("Reset roles"),
+          //           )
+          //         ],
+          //       )
+          //     ],
+          //   ),
+          // ),
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 95.0,
+                    backgroundColor: Color.fromARGB(255, 246, 246, 246),
+                    child: CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 5, 5, 5),
+                      backgroundImage: AssetImage('images/ambi2.png'),
+                      radius: 90.0,
+                    ),
                   ),
-                ),
-                PopupMenuItem(
-                  value: 1,
-                  child: Text("User"),
-                ),
-                PopupMenuItem(
-                  value: 2,
-                  child: Text("Reset roles"),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 95.0,
-                backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                child: CircleAvatar(
-                  backgroundColor: Color.fromARGB(255, 52, 164, 56),
-                  backgroundImage: AssetImage('images/ambi2.png'),
-                  radius: 90.0,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              BorderedText(
-                strokeWidth: 5,
-                strokeColor: Colors.white,
-                child: Text("AmbiNav",
-                style: GoogleFonts.bebasNeue(
-                  fontSize: 40,
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
-                 )
-                ), ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  BorderedText(
+                    strokeWidth: 4,
+                    strokeColor: Color.fromARGB(255, 251, 251, 251),
+                    child: Text("AMBINAV",
+                        style: GoogleFonts.montserrat(
+                          
+                          fontSize: 40,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.5,
+                        
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  
+                  BorderedText(
+                    strokeWidth: 0.8,
+                    strokeColor: Color.fromARGB(255, 0, 0, 0),
+                    child: Text("Choose your role",
+                        style: GoogleFonts.rokkitt(
+                           fontSize: 21,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1,
+                        )),
+                  ),
 
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Choose your Role",
-                style: TextStyle(
-                    color: Colors.green,
-
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17.0,
-                    // fontWeight: FontWeight.bold,
-                    letterSpacing: 1.5),
-              ),
-              SizedBox(
-                height: 40.0,
-                width: 260.0,
-                child: Divider(
-                  color: Color.fromARGB(255, 243, 249, 251),
-                  thickness: 1.6,
-                ),
-              ),
-              Card(
-                borderOnForeground: true,
-                color: Colors.grey[200],
-                elevation: 8.0,
-                shadowColor: Colors.black,
-                child: InkWell(
-                  splashColor: Colors.blue.withAlpha(30),
-                  onTap: () {
-                    debugPrint('Card tapped.');
-                    // Connect to map
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AmbiDriverDetails()));
-                  },
-                  child: const SizedBox(
-                    width: 200,
-                    height: 50,
-                    child: Center(child: Text('Ambulance Driver')),
+                  SizedBox(
+                    height: 40.0,
+                    width: 260.0,
+                    child: Divider(
+                      color: Color.fromARGB(255, 243, 249, 251),
+                      thickness: 1.6,
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Card(
-                color: Colors.grey[200],
-                elevation: 8.0,
-                shadowColor: Color.fromARGB(255, 60, 63, 61),
-                child: InkWell(
-                  splashColor: Color.fromARGB(255, 19, 148, 36).withAlpha(30),
-                  onTap: () {
-                    debugPrint('Card tapped.');
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => userDetails()));
-                    // Connect to map
-                  },
-                  child: const SizedBox(
-                    width: 200,
-                    height: 50,
-                    child: Center(child: Text('User Driver')),
+                  Card(
+                    borderOnForeground: true,
+                    color: Colors.grey[200],
+                    elevation: 8.0,
+                    shadowColor: Color.fromARGB(255, 255, 255, 255),
+                    child: InkWell(
+                      splashColor: Color.fromARGB(255, 25, 143, 96).withAlpha(30),
+                      onTap: () {
+                        debugPrint('Card tapped.');
+                        // Connect to map
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AmbiDriverDetails()));
+                      },
+                      child: const SizedBox(
+                        width: 200,
+                        height: 50,
+                        child: Center(
+                          child: Text('Ambulance Driver',
+                          style: TextStyle(
+                            letterSpacing: 0.7,
+                            fontSize: 16,
+                          ),
+                          ),
+                          ),
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Card(
+                    color: Colors.grey[200],
+                    elevation: 8.0,
+                    shadowColor: Color.fromARGB(255, 247, 248, 247),
+                    child: InkWell(
+                      splashColor:
+                          Color.fromARGB(255, 19, 148, 36).withAlpha(30),
+                      onTap: () {
+                        debugPrint('Card tapped.');
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => userDetails()));
+                        // Connect to map
+                      },
+                      child: const SizedBox(
+                        width: 200,
+                        height: 50,
+                        child: Center(child: Text('User Driver',
+                        style: TextStyle(
+                            letterSpacing: 0.7,
+                            fontSize: 16,
+                          ))),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
