2912af36792c9051003dc2379e3c733095bc166a
Sumedh Hagaldivte <sumedh.hagaldivte@gmail.com>
2022-11-25 15:20:35 +0530

    changed to starter page

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:navigation/AmbDriverDetails.dart';
 import 'package:navigation/userDetails.dart';
 // import 'login_screen.dart';
 import 'get_location.dart';
-import 'loginPage.dart';
+import 'starter.dart';
 //demonstarting git to sumedh
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: userDetails(),
+    home: loginpg(),
   ));
 }
