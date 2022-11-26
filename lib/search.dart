2d3e8f990d516e6ac89364ee3df56a7f02ea7ce2
Amitash M S <amitu895@gmail.com>
2022-11-26 16:59:56 +0530

    Added Place Autocomplete / Suggestions - need to add action on selecting a place

--- lib/search.dart
+++ lib/search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:mapmyindia_place_widget/mapmyindia_place_widget.dart';
 
 class SearchWidget extends StatefulWidget {
   const SearchWidget({super.key});
@@ -7,42 +9,48 @@ class SearchWidget extends StatefulWidget {
   static void toggleVisisbility() {
     isVisible = !isVisible;
   }
-  
+
   @override
   State<SearchWidget> createState() => _SearchWidgetState();
 }
 
 class _SearchWidgetState extends State<SearchWidget> {
-
-  
-  
   @override
   Widget build(BuildContext context) {
     return Visibility(
       child: Container(
-        margin: EdgeInsets.all(30),
-        child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a place',
+          margin: EdgeInsets.all(30),
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter a starting point',
+                  ),
+                  onTap: (() async {
+                    AutocompleteResult x = await openPlaceAutocomplete(
+                        PlaceOptions(
+                            enableTextSearch: true, hint: "Search Location"));
+                  }),
                 ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a destination',
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your destination',
+                  ),
+                  onTap: (() async {
+                    AutocompleteResult x = await openPlaceAutocomplete(
+                        PlaceOptions(
+                            enableTextSearch: true, hint: "Search Location"));
+                  }),
                 ),
-              ),
-            ],
-          ),
-          shadowColor: Colors.lightGreen,
-          elevation: 20,
-        )
-      ),
+              ],
+            ),
+            shadowColor: Colors.lightGreen,
+            elevation: 20,
+          )),
       visible: SearchWidget.isVisible,
     );
   }
