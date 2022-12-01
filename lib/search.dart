638bb37b09858c654f07f2d06767d3b3b7f1ecf1
Rahul K P <rahulkp01aug@gmail.com>
2022-12-01 10:26:16 +0530

    Added boundary to suggested options and camera movement to chose location

--- lib/search.dart
+++ lib/search.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:location/location.dart';
+import 'package:mapmyindia_gl/mapmyindia_gl.dart';
+import 'shared_data.dart';
 import 'package:mapmyindia_place_widget/mapmyindia_place_widget.dart';
 
 class SearchWidget extends StatefulWidget {
@@ -30,9 +33,17 @@ class _SearchWidgetState extends State<SearchWidget> {
                     hintText: 'Enter a starting point',
                   ),
                   onTap: (() async {
+                    LocationData ld = await SharedData.locationData.first;
                     AutocompleteResult x = await openPlaceAutocomplete(
                         PlaceOptions(
-                            enableTextSearch: true, hint: "Search Location"));
+                            enableTextSearch: true,
+                            hint: "Search Location",
+                            filter: ld.toString(),
+                            location: LatLng(ld.latitude!,ld.longitude!)));
+                            // pod: AutoSuggestCriteria.POD_CITY));
+                    
+                    SharedData.mapController.moveCameraWithELoc(CameraELocUpdate.newELocZoom(x.eLocation!.eLoc!, 14));
+                    SharedData.mapController.addSymbol(SymbolOptions(eLoc: x.eLocation!.eLoc));
                   }),
                 ),
                 TextField(
