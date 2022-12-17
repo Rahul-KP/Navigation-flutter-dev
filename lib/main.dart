1a224101d23e30a2889bcfdd9edd92e2c023d87d
Rahul K P <rahulkp01aug@gmail.com>
2022-12-17 15:55:30 +0530

    Removed unnecessary imports and comments from main.dart(MapMyIndia related)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'search.dart';
-// import 'login_screen.dart';
 import 'get_location.dart';
-import 'pref_screen.dart';
-//demonstarting git to sumedh
 void main() {
   runApp(const MaterialApp(
-
     home: Pos(),
-    // home: SearchWidget(),
   ));
 }
