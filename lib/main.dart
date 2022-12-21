3b4e3d6b17350c97a6f3c8f3e808340d77c65925
Amitash M S <amitu895@gmail.com>
2022-12-21 16:23:09 +0530

    Added Here SDK code - need to add SDK to github actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app_screen_ui.dart';
+
+void loadCreds() async {
+  await dotenv.load(fileName: "credentials.env");
+}
 void main() {
+  loadCreds();
   runApp(const MaterialApp(
     home: MapScreen(),
   ));
