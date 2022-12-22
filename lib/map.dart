4a122fede8aaeb51df4b6f03d89d822d98cb9800
Rahul K P <rahulkp01aug@gmail.com>
2022-12-22 13:19:07 +0530

    Initial attempt at loading a map using HereMaps

--- lib/map.dart
+++ lib/map.dart
@@ -1,1 +1,27 @@
 // Code for map widget
+import 'package:flutter/material.dart';
+import 'package:here_sdk/core.dart';
+import 'package:here_sdk/mapview.dart';
+
+class MapWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HERE SDK for Flutter - Hello Map!',
+      home: HereMap(onMapCreated: _onMapCreated),
+    );
+  }
+
+  void _onMapCreated(HereMapController hereMapController) {
+    hereMapController.mapScene.loadSceneForMapScheme(MapScheme.normalDay, (MapError? error) {
+      if (error != null) {
+        print('Map scene not loaded. MapError: ${error.toString()}');
+        return;
+      }
+
+      const double distanceToEarthInMeters = 8000;
+      MapMeasure mapMeasureZoom = MapMeasure(MapMeasureKind.distance, distanceToEarthInMeters);
+      hereMapController.camera.lookAtPointWithMeasure(GeoCoordinates(52.530932, 13.384915), mapMeasureZoom);
+    });
+  }
+}
