9b01e1fbbee3a784e36dc88a4dfea0705b171b3d
Rahul K P <rahulkp01aug@gmail.com>
2022-12-23 15:15:25 +0530

    Added a location indicator, the search button overlay visibility toggling needs to be fixed

--- lib/map.dart
+++ lib/map.dart
@@ -26,5 +26,25 @@ class MapWidget extends StatelessWidget {
     });
 
     SharedData.mapController = hereMapController;
+    _addLocationIndicator(GeoCoordinates(52.530932, 13.384915),LocationIndicatorIndicatorStyle.navigation);
+  }
+
+  void _addLocationIndicator(GeoCoordinates geoCoordinates, LocationIndicatorIndicatorStyle indicatorStyle) {
+    LocationIndicator locationIndicator = LocationIndicator();
+    locationIndicator.locationIndicatorStyle = indicatorStyle;
+
+    // A LocationIndicator is intended to mark the user's current location,
+    // including a bearing direction.
+    // For testing purposes, we create a Location object. Usually, you may want to get this from
+    // a GPS sensor instead.
+    Location location = Location.withCoordinates(geoCoordinates);
+    location.time = DateTime.now();
+    // location.bearingInDegrees = _getRandom(0, 360);
+
+    locationIndicator.updateLocation(location);
+
+    // A LocationIndicator listens to the lifecycle of the map view,
+    // therefore, for example, it will get destroyed when the map view gets destroyed.
+    SharedData.mapController.addLifecycleListener(locationIndicator);
   }
 }
