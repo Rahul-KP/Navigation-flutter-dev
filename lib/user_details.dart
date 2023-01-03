6d4662b660449af91cbee37c0744189acd4f677b
Sumedh Hagaldivte <sumedh.hagaldivte@gmail.com>
2023-01-03 13:54:58 +0530

    Cleared the over flow issues in user_details.dart

--- lib/user_details.dart
+++ lib/user_details.dart
@@ -13,6 +13,7 @@ class userDetails extends StatefulWidget {
 }
 
 class _userDetailsState extends State<userDetails> {
+
   final nameController = TextEditingController();
   final codeController = TextEditingController();
 
@@ -25,141 +26,143 @@ class _userDetailsState extends State<userDetails> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.person,
-                size: 90,
-              ),
-              Text(
-                'User Driver',
-                style: GoogleFonts.bebasNeue(
-                  fontSize: 40,
+          child: SingleChildScrollView (
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 90,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Enter your details',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(color: Colors.green.shade600),
-                    ),
-                    hintText: 'Name',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+                Text(
+                  'User Driver',
+                  style: GoogleFonts.bebasNeue(
+                    fontSize: 40,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  controller: codeController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(color: Colors.green.shade600),
-                    ),
-                    hintText: 'Phone number',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Enter your details',
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: GestureDetector(
-                  onTap: () async {
-                    logindata = await SharedPreferences.getInstance();
-                    String username = nameController.text;
-                    String code = codeController.text;
-
-                    if (username != '' && code != '') {
-                      logindata.setBool('login', false);
-
-                      logindata.setString('username', username);
-                      Fluttertoast.showToast(msg: username);
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => AppScreen()));
-                    }
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        color: Colors.green[400],
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Center(
-                      child: Text(
-                        'Move to Map    🗺️',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1,
-                          fontSize: 16,
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.green.shade600),
+                      ),
+                      hintText: 'Name',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: TextField(
+                    controller: codeController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.green.shade600),
+                      ),
+                      hintText: 'Phone number',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: GestureDetector(
+                    onTap: () async {
+                      logindata = await SharedPreferences.getInstance();
+                      String username = nameController.text;
+                      String code = codeController.text;
+          
+                      if (username != '' && code != '') {
+                        logindata.setBool('login', false);
+          
+                        logindata.setString('username', username);
+                        Fluttertoast.showToast(msg: username);
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => AppScreen()));
+                      }
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: Colors.green[400],
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                        child: Text(
+                          'Move to Map    🗺️',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 26,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Want to change role ?',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Container(
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: ((context) => loginpg())));
-                      },
-                      child: Text(
-                        '  Click here',
-                        style: TextStyle(
-                            color: Colors.blue[600],
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 0.5),
+                SizedBox(
+                  height: 26,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Want to change role ?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
-                ],
-              )
-            ],
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushReplacement(MaterialPageRoute(
+                              builder: ((context) => loginpg())));
+                        },
+                        child: Text(
+                          '  Click here',
+                          style: TextStyle(
+                              color: Colors.blue[600],
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0.5),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
