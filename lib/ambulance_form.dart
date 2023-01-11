0f39aa0e42c079ab33e796c53a87dd54bedbdc47
Amitash M S <amitu895@gmail.com>
2023-01-11 12:22:54 +0530

    Integrated ambulance form with plot-path branch

--- lib/ambulance_form.dart
+++ lib/ambulance_form.dart
@@ -1,23 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-void main() => runApp(BookAmbulance());
-
-class BookAmbulance extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final appTitle = 'Book an Ambulance';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appTitle),
-        ),
-        body: AmbulanceForm(),
-      ),
-    );
-  }
-}
 
 // User defined ambulance form widget
 class AmbulanceForm extends StatefulWidget {
@@ -34,49 +17,74 @@ class AmbulanceFormState extends State<AmbulanceForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: const Icon(Icons.person),
-              hintText: 'Enter patient name',
-              labelText: 'Patient Name',
+    final DatabaseReference ref = FirebaseDatabase.instance.ref('forms');
+    final appTitle = 'Book an Ambulance';
+    TextEditingController patient_name = TextEditingController();
+    TextEditingController age = TextEditingController();
+    TextEditingController preferred_hosp = TextEditingController();
+    String gender;
+    return MaterialApp(
+        title: appTitle,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(appTitle),
             ),
-          ),
-          TextField(
-            decoration: new InputDecoration(
-                icon: const Icon(Icons.numbers_rounded),
-                labelText: 'Enter patient age'),
-            keyboardType: TextInputType.number,
-            inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
-          ),
-          DropdownButton<String>(
-            items: ['Male','Female','Other'].map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (_) {},
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              icon: const Icon(Icons.local_hospital),
-              hintText: 'Enter preferred hospital',
-              labelText: 'Preferred hospital',
-            ),
-          ),
-          new Container(
-              padding: const EdgeInsets.only(left: 150.0, top: 40.0),
-              child: new ElevatedButton(
-                child: const Text("Submit"),
-                onPressed: null,
-              ))
-        ],
-      ),
-    );
+            body: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: patient_name,
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.person),
+                      hintText: 'Enter patient name',
+                      labelText: 'Patient Name',
+                    ),
+                  ),
+                  TextField(
+                    controller: age,
+                    decoration: new InputDecoration(
+                        icon: const Icon(Icons.numbers_rounded),
+                        labelText: 'Enter patient age'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                  ),
+                  DropdownButton<String>(
+                    items: ['Male', 'Female', 'Other'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (_) {
+                      gender = _!;
+                    },
+                  ),
+                  TextField(
+                    controller: preferred_hosp,
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.local_hospital),
+                      hintText: 'Enter preferred hospital',
+                      labelText: 'Preferred hospital',
+                    ),
+                  ),
+                  new Container(
+                      // padding: const EdgeInsets.only(left: 150.0, top: 40.0),
+                      child: new ElevatedButton(
+                    child: const Text("Submit"),
+                    onPressed: () {
+                      ref.set({
+                        "patient_name" : patient_name.text,
+                        "age" : age.text,
+                        "preferred_hospital" : preferred_hosp.text
+                      });
+                    },
+                  ))
+                ],
+              ),
+            )));
   }
 }
