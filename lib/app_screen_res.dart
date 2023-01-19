2ccca688aa3f74a322c74c3e764d5222774d38ac
Rahul K P <rahulkp01aug@gmail.com>
2023-01-19 18:58:51 +0530

    Attempt 1 at listening for ambulance booking form responses

--- lib/app_screen_res.dart
+++ lib/app_screen_res.dart
@@ -1,6 +1,7 @@
 import 'package:AmbiNav/navig_notif_overlay_ui.dart';
 import 'package:AmbiNav/search_overlay_ui.dart';
 import 'package:AmbiNav/starter.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -62,6 +63,15 @@ class MapScreenRes {
       ));
     }
     return drawerButtonList;
+  }
+
+  static void listenToBookings() {
+    DatabaseReference ref = FirebaseDatabase.instance.ref("Bookings");
+    ref.onChildAdded.listen((event){
+      for (var element in event.snapshot.children) {
+        print(element);
+      }
+    });
   }
 
   static void search() async {
--- lib/app_screen_ui.dart
+++ lib/app_screen_ui.dart
@@ -17,6 +17,10 @@ class _AppScreenState extends State<AppScreen> {
   void initState() {
     super.initState();
     Services.mapContext = this.context;
+    if(Services.usertype=="driver") {
+      MapScreenRes.listenToBookings();
+    }
+    Services.getPermissions();
   }
 
   //used to reference setState() for search widget (setState is copied to this variable in StatefulBuilder)
