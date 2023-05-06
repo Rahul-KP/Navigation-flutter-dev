a62c0f6a69aa493bb89c1cb2b5ee92b54921dde8
Sumedh Hagaldivte <sumedh.hagaldivte@gmail.com>
2023-05-06 13:49:01 +0530

    bottomsheet to display address is shown but need to remove the button that displays it

--- lib/marker_details_ui.dart
+++ lib/marker_details_ui.dart
@@ -16,17 +16,60 @@ class DisplayMarkerInfo extends StatefulWidget {
 class _DisplayMarkerInfoState extends State<DisplayMarkerInfo> {
   @override
   Widget build(BuildContext context) {
-    return Visibility(
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-            height: 100,
-            child: ListTile(
-          title: Text(Search.place),
-          subtitle: Text(Search.vicinity),
-        )),
+    // return Visibility(
+    //   child: Align(
+    //     alignment: Alignment.bottomCenter,
+    //     child: Container(
+    //         height: 100,
+    //         child: ListTile(
+    //       title: Text(Search.place),
+    //       subtitle: Text(Search.vicinity),
+    //     )),
+    //   ),
+    //   visible: DisplayMarkerInfo.isVisible,
+    // );
+    return Center(
+      child: ElevatedButton(
+        child: const Text('showBottomSheet'),
+        onPressed: () {
+          Scaffold.of(context).showBottomSheet<void>(
+            (BuildContext context) {
+              return Container(
+                height: 180,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20.0),
+                      topLeft: Radius.circular(20.0)),
+                  color: Color.fromARGB(255, 159, 211, 214),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Align(
+                        // alignment: Alignment.bottomCenter,
+                        child: Container(
+                            height: 80,
+                            child: ListTile(
+                              title: Text(Search.place),
+                              subtitle: Text(Search.vicinity),
+                            )),
+                      ),
+                      ElevatedButton(
+                        child: const Text('Close BottomSheet'),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
-      visible: DisplayMarkerInfo.isVisible,
     );
   }
 }
