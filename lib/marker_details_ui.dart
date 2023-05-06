dc88267f7a0fbb93ac758217df6f985768c10a33
Sumedh Hagaldivte <sumedh.hagaldivte@gmail.com>
2023-05-06 15:56:40 +0530

    attempt to make the 'Address!' button toggle but failed

--- lib/marker_details_ui.dart
+++ lib/marker_details_ui.dart
@@ -3,10 +3,19 @@ import 'package:flutter/material.dart';
 
 class DisplayMarkerInfo extends StatefulWidget {
   static bool isVisible = false;
+  static bool addInfo = false;
 
   DisplayMarkerInfo({super.key});
   static void toggleVisisbility() {
     isVisible = !isVisible;
+  }
+
+  static addressInfo(addInfo) {
+    if (addInfo) {
+      addInfo = false;
+    } else {
+      addInfo = true;
+    }
   }
 
   @override
@@ -22,53 +31,59 @@ class _DisplayMarkerInfoState extends State<DisplayMarkerInfo> {
         child: ElevatedButton(
           child: const Text('Address!'),
           onPressed: () {
-           Scaffold.of(context).showBottomSheet<void>(
-            shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only( 
-              topLeft: Radius.circular(25.0),
-              topRight: Radius.circular(25.0),
-            ),
-          ),
-              (BuildContext context) {
-                return Container(
-                  height: 180,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20.0),
-                        topLeft: Radius.circular(20.0)),
-                     color: Color.fromARGB(255, 159, 211, 214),
+            DisplayMarkerInfo.addressInfo(DisplayMarkerInfo.addInfo);
+            if (DisplayMarkerInfo.addInfo) {
+              //show info
+              Scaffold.of(context).showBottomSheet<void>(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25.0),
+                    topRight: Radius.circular(25.0),
                   ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Align(
-                          // alignment: Alignment.bottomCenter,
-                          child: Container(
-                              height: 80,
-                              child: ListTile(
-                                title: Text(Search.place),
-                                subtitle: Text(Search.vicinity),
-                              )),
-                        ),
-                        ElevatedButton(
-                          child: const Text('Close BottomSheet'),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
+                ),
+                (BuildContext context) {
+                  return Container(
+                    height: 180,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20.0),
+                          topLeft: Radius.circular(20.0)),
+                      color: Color.fromARGB(255, 159, 211, 214),
                     ),
-                  ),    
-                );
-              },
-            );
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Align(
+                            // alignment: Alignment.bottomCenter,
+                            child: Container(
+                                height: 80,
+                                child: ListTile(
+                                  title: Text(Search.place),
+                                  subtitle: Text(Search.vicinity),
+                                )),
+                          ),
+                          ElevatedButton(
+                            child: const Text('Close BottomSheet'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            else{
+              Navigator.pop(context);
+            }
           },
         ),
       ),
       visible: DisplayMarkerInfo.isVisible,
     );
-  
   }
 }
